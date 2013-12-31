9bc9ef548134ed529f1ad05d348d2536a70b252d
Tim DuBois <genesis@neophilus.net>
2013-12-31 17:51:08 +1100

    ws.message can now spin the indicator

--- eotserver/static/chadburn.js
+++ eotserver/static/chadburn.js
@@ -1,72 +1,50 @@
 window.onload = function () {
-    var eot = Snap.select("#EOT"),
-        indicator = eot.select("#indicator"),
-        handle = eot.select("#handle"),
+    var eot = Snap.select("#EOT");
 
-        timer;
-
-    var pivot = [1400, 1400];
-            
-    function stop() {
-        clearTimeout(timer);
-
-        indicator.animate({ 
-            transform: "r" + [0, pivot]
-        }, 1000, mina.backout);
-                
-        handle.animate({ 
-            transform: "r" + [0, pivot]
-        }, 1000, mina.backout);
-                
-    }
-
-    function spin(a) {
-        clearTimeout(timer);
-
-        indicator.animate({ 
-            transform: "r" + [a, pivot]
-        }, 1000, mina.backout);
-                
-        handle.animate({ 
-            transform: "r" + [a, pivot]
-        }, 1000, mina.backout);
-                
-    }
-
-    timer = setTimeout(stop, 50);
-            
     eot.select("#stop-area").click(function () {
-        spin(0);
+        spin(0,1);
     });
     eot.select("#coffee-area").click(function () {
-        spin(20.84);
+        spin(20.84,1);
     });
     eot.select("#lunch-area").click(function () {
-        spin(41.68);
+        spin(41.68,1);
     });
     eot.select("#eww-area").click(function () {
-        spin(62.52);
+        spin(62.52,1);
     });
     eot.select("#siwoti-area").click(function () {
-        spin(83.36);
+        spin(83.36,1);
     });
     eot.select("#battle-area").click(function () {
-        spin(104.20);
+        spin(104.20,1);
     });
     eot.select("#next-area").click(function () {
-        spin(-20.84);
+        spin(-20.84,1);
     });
     eot.select("#reverse-area").click(function () {
-        spin(-41.68);
+        spin(-41.68,1);
     });
     eot.select("#slow-area").click(function () {
-        spin(-62.52);
+        spin(-62.52,1);
     });
     eot.select("#half-area").click(function () {
-        spin(-83.36);
+        spin(-83.36,1);
     });
     eot.select("#full-area").click(function () {
-        spin(-104.20);
+        spin(-104.20,1);
     });
 };
 
+function spin(angle, type) {
+    Snap.select("#EOT").select("#indicator").animate({ 
+        transform: "r" + [angle, 1400, 1400]
+    }, 1000, mina.backout);
+
+    if (type === 1) {        
+        Snap.select("#EOT").select("#handle").animate({ 
+            transform: "r" + [angle, 1400, 1400]
+        }, 1000, mina.backout);
+    }
+}
+            
--- eotserver/static/socket.js
+++ eotserver/static/socket.js
@@ -4,6 +4,9 @@ ws.onmessage = function(evt){
     x = document.createElement("p");
     x.innerHTML = evt.data;
     document.getElementById("console").appendChild(x);
+    if (evt.data === "2") {
+        spin(180, 0);
+    }
 }
    
 function DispatchResponse(){
