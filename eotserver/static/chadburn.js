222ffca4b266a2f58ad045266871fdbcc7a3909d
Tim DuBois <genesis@neophilus.net>
2013-12-31 17:04:24 +1100

    All clickable areas spin as expected

--- eotserver/static/chadburn.js
+++ eotserver/static/chadburn.js
@@ -20,24 +20,53 @@ window.onload = function () {
                 
     }
 
-    function spin() {
+    function spin(a) {
         clearTimeout(timer);
 
         indicator.animate({ 
-            transform: "r" + [105, pivot]
+            transform: "r" + [a, pivot]
         }, 1000, mina.backout);
                 
         handle.animate({ 
-            transform: "r" + [-105, pivot]
+            transform: "r" + [a, pivot]
         }, 1000, mina.backout);
                 
     }
 
     timer = setTimeout(stop, 50);
             
+    eot.select("#stop-area").click(function () {
+        spin(0);
+    });
+    eot.select("#coffee-area").click(function () {
+        spin(20.84);
+    });
+    eot.select("#lunch-area").click(function () {
+        spin(41.68);
+    });
+    eot.select("#eww-area").click(function () {
+        spin(62.52);
+    });
+    eot.select("#siwoti-area").click(function () {
+        spin(83.36);
+    });
+    eot.select("#battle-area").click(function () {
+        spin(104.20);
+    });
+    eot.select("#next-area").click(function () {
+        spin(-20.84);
+    });
+    eot.select("#reverse-area").click(function () {
+        spin(-41.68);
+    });
+    eot.select("#slow-area").click(function () {
+        spin(-62.52);
+    });
+    eot.select("#half-area").click(function () {
+        spin(-83.36);
+    });
     eot.select("#full-area").click(function () {
-            spin();
-            timer = setTimeout(stop, 1300);
+        spin(-104.20);
     });
 };
 
